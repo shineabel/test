79bf8b98cc132cca658e703ff45f24aad6a5a8b0
xianming <531088309@qq.com>
2019-11-07 11:39:39 +0800

    test

--- write_data.py
+++ write_data.py
@@ -5,7 +5,7 @@ from pymysql import connect
 
 class MySQL(object):
     def __init__(self):
-        self.conn = connect(host='172.16.0.31', user='root', password='test', database='test', charset='utf8')
+        self.conn = connect(host='172.16.0.31', user='root', password='deakingreport', database='report', charset='utf8')
         self.cursor = self.conn.cursor()
 
     def __del__(self):
@@ -21,31 +21,15 @@ class MySQL(object):
             print("Error sql:" + sql)
             self.conn.rollback()
 
-    def insert_sql(self, name, key, current_time):
-        sql = "insert into smp(name, key, current_time) values('%s', '%s','%s');" % (name, key, current_time)
+    def insert_sql(self, name, key):
+        sql = "insert into test(k, v) values('%s', '%s');" % (name, key)
+        print(sql)
         self.execute_sql(sql)
 
 
 def main():
     db = MySQL()
-    with serial.Serial('/dev/ttyACM2', 9600, timeout=1) as ser:
-        while True:
-            try:
-                data = ser.readline('utf-8')
-                time.sleep(0.5)
-                if data == '':
-                    continue
-                else:
-                    data = data.split()
-                    current_time = data[0]
-                    name = data[1]
-                    key = data[2]
-                    print("{} {} {}".format(name, key, current_time))
-                    db.insert_sql(name, key, current_time)
-            except IOError as e:
-                print("error:", e)
-                ser.close()
-                ser.flush()
+    db.insert_sql("test1", "test1")
 
 
 if __name__ == '__main__':
